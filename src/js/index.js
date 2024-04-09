79e3ca484988e65718abaf1a69850a42ff069267
r.vinichenko <r.vinichenko@disoft.dev>
2024-04-09 16:07:40 +0300

    qa1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Header Scroll
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   const header = document.getElementById('header');
   if (window.scrollY > 20) {
     header.classList.add('header__active');
@@ -7,7 +7,6 @@ window.addEventListener('scroll', function() {
     header.classList.remove('header__active');
   }
 });
-
 
 // Accordion
 const faqs = document.querySelectorAll('.faq__item');
@@ -38,7 +37,25 @@ faqs.forEach((accordion) => {
   };
 });
 
-document.getElementById('mobile').addEventListener('click', function() {
+
+// Dropdown
+document.querySelector('.nav-drop').addEventListener('click', function () {
+  this.classList.toggle('nav-drop__active');
+});
+
+// Burger Menu
+document.getElementById('mobile').addEventListener('click', function () {
   document.querySelector('.nav__box').classList.toggle('nav__box-active');
   document.querySelector('.nav__mobile').classList.toggle('nav__mobile-active');
+  document.body.classList.toggle('hidden');
+});
+
+var navLinks = document.querySelectorAll('.nav-list a');
+
+navLinks.forEach(function (navLink) {
+  navLink.addEventListener('click', function () {
+    document.querySelector('.nav__box').classList.remove('nav__box-active');
+    document.querySelector('.nav__mobile').classList.remove('nav__mobile-active');
+    document.body.classList.remove('hidden');
+  });
 });
